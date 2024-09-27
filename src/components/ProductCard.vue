d0848825a8228b4990e2f6c92b233cb0c3f181a5
thanapat <ballgaess12@gmail.com>
2024-09-28 04:31:40 +0700

    move position @click->goToProductDetail

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,8 +1,8 @@
 
 <template>
   <div class="p-4 rounded-lg text-center transition-all duration-300 hover:scale-105 hover:bg-stone-100">
-  <div >
-		<img @click="goToProductDetail" class="w-full h-64 object-cover rounded-t-lg" :src="product.image" :alt="product.name" />
+  <div  @click="goToProductDetail">
+		<img class="w-full h-64 object-cover rounded-t-lg" :src="product.image" :alt="product.name" />
 		<h3 class="mt-4 text-red-600 uppercase text-sm text-left pl-1">{{ product.brand }}</h3>
 		<p class="text-xl font-thin mt-1 text-left pl-1 text-black">{{ product.name }}</p>
 		<p class="text-xl font-thin pl-1 mt-3 text-white bg-red-500 p-1 rounded-3xl text-center">{{ product.price }} THB</p>
@@ -11,15 +11,10 @@
 </template>
 
 <script>
-import { useCartStore } from '@/stores/cart';
-
 export default {
   name: 'ProductCard',
   props: {
     product: Object,
-  },
-  created() {
-    this.cartStore = useCartStore();
   },
   methods: {
     goToProductDetail() {
