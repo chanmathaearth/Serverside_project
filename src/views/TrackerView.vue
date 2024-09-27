4ac915cdc2faf85fd32851833eef9376f655bded
chanmathaearth <chanmathaearth@gmail.com>
2024-09-27 20:55:31 +0700

    add tracker

---
+++ src/views/TrackerView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="text-center">
+        <button @click="count++" class="btn">Add</button>
+        <p class="text-black">Count is: {{ count.value }}</p>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue'
+    const count = ref(0)
+</script>
+
+<style>
+body {
+font-family: "Changa", sans-serif;
+}
+</style>
