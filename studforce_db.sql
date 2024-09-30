45a03fdc10346ff3e5fc6e7cdd4c6820bd9706e5
chanmathaearth <chanmathaearth@gmail.com>
2024-10-01 02:57:36 +0700

    add backend

---
+++ studforce_db.sql
@@ -0,0 +1,170 @@
+INSERT INTO studforce_customer_productcategory (name) VALUES ('speed');
+INSERT INTO studforce_customer_product (name, description, brand, price, color, image, amount)
+VALUES 
+        ('ASICS DS LIGHT X-FLY PRO 2 PARIS'
+        , 'The DS LIGHT X-FLY™ PRO 2 PARIS cleat is designed for soccer players seeking soccer cleats that work on and off the ball. This shoe helps athletes generate faster acceleration after stopping and transitioning into vertical movements. This colorway is inspired by "the city of lights" and its energizing tones at sunrise and sunset.
+        This shoe features a softer kangaroo leather forefoot.
+        Complemented with an acceleration stud design in the outsole, this shoe also delivers a better grip during straight dashes on the pitch.'
+        , 
+        'Asics', 7500, 'Yellow', 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/a/s/asics_ds_light_x-fly_pro_2_paris_1101a074.750__01.jpg'
+        , 15);
+
+INSERT INTO studforce_customer_product_categories (product_id, productcategory_id) VALUES (1, 1);
+
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (1, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/a/s/asics_ds_light_x-fly_pro_2_paris_1101a074.750__02.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (1, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/a/s/asics_ds_light_x-fly_pro_2_paris_1101a074.750__03.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (1, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/a/s/asics_ds_light_x-fly_pro_2_paris_1101a074.750__04.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (1, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/a/s/asics_ds_light_x-fly_pro_2_paris_1101a074.750__05.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (1, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/a/s/asics_ds_light_x-fly_pro_2_paris_1101a074.750__06.jpg');
+
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '40.5', 1);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '41.5', 1);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '42', 1);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '42.5', 1);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '43.5', 1);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '44', 1);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '45', 1);
+
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '7.5', 1);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '8', 1);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '8.5', 1);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '9', 1);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '9.5', 1);
+
+
+
+
+INSERT INTO studforce_customer_productcategory (name) VALUES ('control');
+INSERT INTO studforce_customer_product (name, description, brand, price, color, image, amount)
+VALUES 
+        ('PUMA FUTURE 7 ULTIMATE VOLUME'
+        ,'Make some noise with Volume Up! This pack features bold and brilliant takes on the 
+        latest ULTRA 5 ULTIMATE. We take a playful approach to branding and color blocking,
+         celebrating creativity, freedom, and self-expression on the football pitch.Playmakers, 
+         lock into the FUTURE. The re-engineered upper combines PWRPRINT, PWRTAPE, extra stretchy 
+         knit, and soft dual mesh for a next-generation adaptive fit, so you can create without 
+         constraints throughout the 90 minutes. Dynamic Motion System provides'
+        , 
+        'Puma', 8000, 'White', 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/f/u/future_7_ultimate_vol._up_fg_ag_10807201__01.jpg'
+        , 15);
+
+INSERT INTO studforce_customer_product_categories (product_id, productcategory_id) VALUES (2, 2);
+
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (2, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/f/u/future_7_ultimate_vol._up_fg_ag_10807201__02.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (2, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/f/u/future_7_ultimate_vol._up_fg_ag_10807201__03.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (2, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/f/u/future_7_ultimate_vol._up_fg_ag_10807201__04.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (2, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/f/u/future_7_ultimate_vol._up_fg_ag_10807201__05.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (2, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/f/u/future_7_ultimate_vol._up_fg_ag_10807201__06.jpg');
+
+
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '40.5', 2);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '41.5', 2);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '42', 2);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '42.5', 2);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '43.5', 2);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '44', 2);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '45', 2);
+
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '7.5', 2);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '8', 2);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '8.5', 2);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '9', 2);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '9.5', 2);
+
+
+INSERT INTO studforce_customer_productcategory (name) VALUES ('touch');
+INSERT INTO studforce_customer_product (name, description, brand, price, color, image, amount)
+VALUES 
+        ('MIZUNO MORELIA II PRO AG VIETNAMP'
+        ,'This boot is made specially for use on artificial grass. The new AG sole is about 10 grams lighter than the previous AG model. It provides moderate grip that won‘t leave you stuck in the grass.'
+        , 
+        'Mizuno', 4500, 'Black', 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/m/i/mizuno_morelia_ii_pro_ag_-_black_galaxy_silver_dark_shadow_p1ga241403__01.jpg'
+        , 15);
+
+INSERT INTO studforce_customer_product_categories (product_id, productcategory_id) VALUES (3, 3);
+
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (3, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/m/i/mizuno_morelia_ii_pro_ag_-_black_galaxy_silver_dark_shadow_p1ga241403__02.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (3, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/m/i/mizuno_morelia_ii_pro_ag_-_black_galaxy_silver_dark_shadow_p1ga241403__03.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (3, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/m/i/mizuno_morelia_ii_pro_ag_-_black_galaxy_silver_dark_shadow_p1ga241403__04.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (3, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/m/i/mizuno_morelia_ii_pro_ag_-_black_galaxy_silver_dark_shadow_p1ga241403__05.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (3, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/m/i/mizuno_morelia_ii_pro_ag_-_black_galaxy_silver_dark_shadow_p1ga241403__06.jpg');
+
+
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '40.5', 3);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '41.5', 3);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '42', 3);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '42.5', 3);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '43.5', 3);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '44', 3);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '45', 3);
+
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '7.5', 3);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '8', 3);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '8.5', 3);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '9', 3);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '9.5', 3);
+
+
+INSERT INTO studforce_customer_productcategory (name) VALUES ('touch');
+INSERT INTO studforce_customer_product (name, description, brand, price, color, image, amount)
+VALUES 
+        ('ADIDAS F50 + FG - TURBO/AURORA'
+        ,'ปลดปล่อยความไวไปกับ adidas F50 สร้างมาเพื่อปลดล็อกขีดสุดของศักยภาพด้านการเร่งสปีด การเคลื่อนไหว และความไว รองเท้าฟุตบอลอาดิดาสรุ่น + คู่นี้เพิ่มประสิทธิภาพมากขึ้นจากรองเท้า F50 ด้วยชุดพื้นชั้นล่าง Sprintframe 360+ ที่มาพร้อมแผ่นรองแบบสองชั้นที่ปลายรองเท้าและแผ่นรองรับแรงกระแทก Lightstrike Pro อัปเปอร์ Fibertouch มาพร้อมลิ้นรองเท้า Compression Fit Tunnel ที่ให้ความกระชับเท้าและผิวสัมผัส Sprintweb แบบสามมิติเพื่อการเลี้ยงบอลที่ว่องไว'
+        , 
+        'Adidas', 10500, 'Red', 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/a/d/adidas_f50_fg_if1275__01.jpg'
+        , 15);
+
+INSERT INTO studforce_customer_product_categories (product_id, productcategory_id) VALUES (4, 3);
+
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (4, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/a/d/adidas_f50_fg_if1275__02.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (4, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/a/d/adidas_f50_fg_if1275__03.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (4, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/a/d/adidas_f50_fg_if1275__04.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (4, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/a/d/adidas_f50_fg_if1275__05.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (4, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/a/d/adidas_f50_fg_if1275__06.jpg');
+
+
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '40.5', 4);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '41.5', 4);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '42', 4);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '42.5', 4);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '43.5', 4);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '44', 4);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '45', 4);
+
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '7.5', 4);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '8', 4);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '8.5', 4);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '9', 4);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '9.5', 4);
+
+INSERT INTO studforce_customer_productcategory (name) VALUES ('speed');
+INSERT INTO studforce_customer_product (name, description, brand, price, color, image, amount)
+VALUES 
+        ('NIKE MERCURIAL SUPERFLY 10'
+        ,'Obsessed with speed? So are the game''s biggest stars. That''s why we made these Elite cleats with an improved 3/4-length Air Zoom unit. It gives you and the sport''s fastest players the propulsive feel needed to break through the back line. The result is the most responsive Mercurial we''ve ever made, because you demand greatness from yourself and your footwear.'
+        , 
+        'Nike', 9600, 'Blue', 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/z/m/zm_superfly_10_elite_fg_fq1454-400__01.jpg'
+        , 15);
+
+INSERT INTO studforce_customer_product_categories (product_id, productcategory_id) VALUES (5, 1);
+
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (5, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/z/m/zm_superfly_10_elite_fg_fq1454-400__02.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (5, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/z/m/zm_superfly_10_elite_fg_fq1454-400__03.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (5, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/z/m/zm_superfly_10_elite_fg_fq1454-400__04.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (5, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/z/m/zm_superfly_10_elite_fg_fq1454-400__05.jpg');
+INSERT INTO studforce_customer_productimage (product_id, image) VALUES (5, 'https://www.arifootballstore.com/media/catalog/product/cache/6e478a31517304dced53ac4d3f3d5560/z/m/zm_superfly_10_elite_fg_fq1454-400__06.jpg');
+
+
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '40.5', 5);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '41.5', 5);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '42', 5);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '42.5', 5);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '43.5', 5);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '44', 5);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('EUR', '45', 5);
+
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '7.5', 5);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '8', 5);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '8.5', 5);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '9', 5);
+INSERT INTO studforce_customer_productsize (type_size, size, product_id) VALUES ('US', '9.5', 5);
